2cb53455a6c3753a17fd8ae0240ee25c0a572819
夜栖幕晨霜 <123211496@qq.com>
2019-01-21 17:17:47 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const  UrlName=""; //配置地址
+const  header={    //配置请求头
+  'Accept': 'application/json',
+  'content-type': 'application/json',
+  'Authorization': null,
+}
+function get(url,data,call){
+	wx.hideLoading({
+		title:'加载中',
+	})
+	wx.request({
+		url:UrlName+url;
+		method:'get',
+		header:header,
+		success:function(res=>{
+			 wx.hideLoading();
+		},
+		fail: function () {
+      wx.hideLoading();
+      wx.showModal({
+        title: '网络错误',
+        content: '网络出错，请刷新重试',
+        showCancel: false
+      })
+    }
+	)
+	})
+}
+
+
+module.exports={
+	get:get
+}
